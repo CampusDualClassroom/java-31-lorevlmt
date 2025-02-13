61e5212cadb0914b5219dfa46656ef6119e972cf
lorevlmt <lore.vimontes@gmail.com>
2025-02-13 09:18:55 +0100

    Ejercicio 31 mejorado

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,19 +1,19 @@
 package com.campusdual.classroom;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Exercise31 {
 
     public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new FileReader(new File("src/main/resources/lorem.txt")))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/lorem.txt"))) {
             String linea;
             while ((linea = br.readLine()) != null) {
                 System.out.println(linea);
             }
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
